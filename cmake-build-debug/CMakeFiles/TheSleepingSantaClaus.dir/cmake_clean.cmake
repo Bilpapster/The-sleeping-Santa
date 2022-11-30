340898b75093be4550d4b9acfe7ca6caf1b76854
Vasileios Papastergios <papastva@csd.auth.gr>
2022-12-01 00:17:07 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TheSleepingSantaClaus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TheSleepingSantaClaus.dir/main.cpp.obj"
+  "TheSleepingSantaClaus.exe"
+  "TheSleepingSantaClaus.exe.manifest"
+  "TheSleepingSantaClaus.pdb"
+  "libTheSleepingSantaClaus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TheSleepingSantaClaus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
